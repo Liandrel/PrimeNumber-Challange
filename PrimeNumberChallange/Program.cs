88add59fd3c5325ec8dc637a953ca9490ff452f8
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-12 19:44:33 +0200

    Add project files.

---
+++ PrimeNumberChallange/Program.cs
@@ -0,0 +1,104 @@
+﻿using PrimeNumberLibrary;
+using System.Text;
+
+
+
+
+NumberModel numberToTest = new(AskUserForNumber());
+
+PrimeCheck(numberToTest);
+
+FactorsOfNumberCheck(numberToTest);
+
+PrimeFactorsOfNumberCheck(numberToTest);
+
+LargestPrimeFactorCheck(numberToTest);
+
+Console.ReadLine();
+
+
+
+
+
+
+
+int AskUserForNumber()
+{
+    int output;
+    bool isIntValid = false;
+    do
+    {
+        Console.Write("Write number: ");
+        string numberString = Console.ReadLine();
+        isIntValid = int.TryParse(numberString, out output);
+
+        if (isIntValid == false)
+        {
+            Console.WriteLine("Please write a valid number !");
+        }
+    } while (isIntValid == false);
+
+    return output;
+}
+
+void PrimeCheck(NumberModel number)
+{
+    if (number.IsPrime == true)
+    {
+        Console.WriteLine($"{number.Value} is a Prime Number");
+    }
+    else
+    {
+        Console.WriteLine($"{number.Value} is not a Prime Number");
+    }
+    Console.WriteLine();
+}
+
+void FactorsOfNumberCheck(NumberModel number)
+{
+    if(number.Factors.Count == 0)
+    {
+        Console.WriteLine($"Number {number.Value} has no factors.");
+    }
+    else
+    {
+        Console.WriteLine($"List of factors of number {number.Value}: ");
+        StringBuilder sb = new();
+        foreach (var factor in number.Factors)
+        {
+            sb.Append($"{factor.Value}, ");
+        }
+        sb.Remove(sb.Length - 2, 2);
+        Console.WriteLine($"{sb}");
+        Console.WriteLine();
+    }
+}
+
+void PrimeFactorsOfNumberCheck(NumberModel number)
+{
+    if(number.PrimeFactors.Count != 0)
+    {
+        Console.WriteLine($"List of Prime factors of number {number.Value}: ");
+        StringBuilder sb = new();
+        foreach (var factor in number.PrimeFactors)
+        {
+            sb.Append($"{factor.Value}, ");
+        }
+        sb.Remove(sb.Length - 2, 2);
+        Console.WriteLine($"{sb}");
+        Console.WriteLine();
+    }
+}
+
+void LargestPrimeFactorCheck(NumberModel number)
+{
+    if(number.PrimeFactors.Count != 0)
+    {
+        Console.Write($"Largest Prime factor of number {number.Value}: ");
+        Console.WriteLine(number.PrimeFactors.Max()?.Value);
+        Console.WriteLine();
+    }
+}
+
+
+
---
+++ PrimeNumberLibrary/NumberModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberLibrary
+{
+    public class NumberModel :IComparable
+    {
+        public int Value { get; set; }
+        public bool IsPrime { get; set; }
+        public List<NumberModel> Factors { get; set; } = new();
+        public List<NumberModel> PrimeFactors { get; set; } = new();
+        
+        public NumberModel(int value)
+        {
+            Value = value;
+            IsPrime = IsNumberPrime(Value);
+            Factors = AllFactorsOfNumber(Value);
+            PrimeFactors = PrimeFactorsOfNumber(Factors);
+        }
+
+
+
+        private static bool IsNumberPrime(int number)
+        {
+            if ((number == 2) || (number == 3))
+            {
+                return true;
+            }
+            if ((number <= 1) || (number % 2 == 0) || (number % 3 == 0))
+            {
+                return false;
+            }
+            for (long i = 5; i * i <= number; i += 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static List<NumberModel> AllFactorsOfNumber(int number)
+        {
+            List<NumberModel> output = new();
+
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    output.Add(new NumberModel(i));
+                }
+            }
+
+            return output;
+        }
+
+        private static List<NumberModel> PrimeFactorsOfNumber(List<NumberModel> factors)
+        {
+            List<NumberModel> output = new();
+
+            foreach (NumberModel factor in factors)
+            {
+                if(factor.IsPrime == true)
+                {
+                    output.Add(factor);
+                }
+            }
+            return output;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            NumberModel? nextNumber = obj as NumberModel;
+
+            return this.Value.CompareTo(nextNumber?.Value);
+        }
+    }
+}
